de96fd3755e891d2e1451d738d919faa944aa6c3
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2024-05-18 22:15:53 +0300

    Added navigation and landing fragment

---
+++ app/src/main/java/com/example/movies/LandingFragment.kt
@@ -0,0 +1,16 @@
+package com.example.movies
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LandingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_landing, container, false)
+    }
+}
