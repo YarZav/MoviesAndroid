9405d60f3b4b0424dca18a00a093771f4615c233
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2024-05-18 23:57:04 +0300

    Added landing info

--- app/src/main/java/com/example/movies/LandingFragment.kt
+++ app/src/main/java/com/example/movies/LandingFragment.kt
@@ -1,16 +1,27 @@
 package com.example.movies
 
+import android.util.Log
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.movies.databinding.FragmentLandingBinding
 
 class LandingFragment : Fragment() {
+    private lateinit var binding: FragmentLandingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_landing, container, false)
+        binding = FragmentLandingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        
     }
 }
