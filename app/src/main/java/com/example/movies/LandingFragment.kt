b84d46d3e52eefd2a11b4fb5496759133e41d058
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2024-05-19 18:23:30 +0300

    Added landing fragment content

--- app/src/main/java/com/example/movies/LandingFragment.kt
+++ app/src/main/java/com/example/movies/LandingFragment.kt
@@ -1,12 +1,12 @@
 package com.example.movies
 
-import android.util.Log
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.movies.databinding.FragmentLandingBinding
+
 
 class LandingFragment : Fragment() {
     private lateinit var binding: FragmentLandingBinding
@@ -16,12 +16,24 @@ class LandingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLandingBinding.inflate(layoutInflater)
+
+        binding.root.post(Runnable {
+            val rootWidth = binding.root.measuredWidth
+
+            val params = binding.landingOneWeek.layoutParams
+            params.width = (rootWidth - 18 * 2) / 3
+            binding.landingOneWeek.setLayoutParams(params)
+        })
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        
+        binding.buttonLearnMore.setOnClickListener {
+            val childView = binding.landingAboutProjectBlock
+            binding.fragmentLandingScroll.smoothScrollTo(0, childView.top);
+        }
     }
 }
