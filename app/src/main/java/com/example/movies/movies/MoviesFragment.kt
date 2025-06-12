782353448795f864522004a6e988bbaec6a6bf46
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2025-06-12 20:39:02 +0300

    Added menu item style

--- app/src/main/java/com/example/movies/movies/MoviesFragment.kt
+++ app/src/main/java/com/example/movies/movies/MoviesFragment.kt
@@ -1,14 +1,22 @@
 package com.example.movies.movies
 
+import android.R.string
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.style.LineBackgroundSpan
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import androidx.transition.TransitionInflater
 import com.example.movies.R
 import com.example.movies.databinding.FragmentMoviesBinding
+
 
 class MoviesFragment : Fragment() {
     private lateinit var binding: FragmentMoviesBinding
@@ -23,7 +31,7 @@ class MoviesFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentMoviesBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -36,5 +44,60 @@ class MoviesFragment : Fragment() {
         binding.toolbar.setNavigationOnClickListener {
             binding.drawerLayout.openDrawer(GravityCompat.START)
         }
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+               R.id.general -> {
+                   underlineMenuItem(menuItem)
+                   menuItem.isChecked = true
+               }
+            }
+
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
+    private fun underlineMenuItem(menuItem: MenuItem) {
+        val spannableString = SpannableString(menuItem.title)
+        spannableString.setSpan(CustomUnderlineSpan(), 0, spannableString.length, 0)
+        menuItem.title = spannableString
+    }
+}
+
+class CustomUnderlineSpan(
+    private val underlineThickness: Float = 3f,
+    private val underlineSpace: Int = 7
+): LineBackgroundSpan {
+
+    override fun drawBackground(
+        canvas: Canvas,
+        paint: Paint,
+        left: Int,
+        right: Int,
+        top: Int,
+        baseline: Int,
+        bottom: Int,
+        text: CharSequence,
+        start: Int,
+        end: Int,
+        lnum: Int
+    ) {
+        val textPaint = paint as TextPaint
+        val originalColor = textPaint.color
+        textPaint.color = textPaint.color
+        textPaint.strokeWidth = underlineThickness
+
+        val right = paint.measureText(text.toString())
+        canvas.drawLine(
+            left.toFloat(),
+            bottom.toFloat() + underlineSpace,
+            right,
+            bottom.toFloat() + underlineSpace,
+            textPaint
+        )
+
+        textPaint.color = originalColor
+        textPaint.strokeWidth = underlineThickness
     }
 }
